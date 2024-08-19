053125e27cbac2c193c9130484eab4607413574f
aldrin1122 <0318-1190@lspu.edu.ph>
2024-08-19 15:44:19 +0800

    done

---
+++ dist/js/slider.js
@@ -0,0 +1,10 @@
+var feedBack = tns ({
+    container: ".feedback__slider",
+    items: 1,
+    gutter: 100,
+    slideBy: "page",
+    autoplay: true,
+    controls: false,
+    nav: false,
+    autoplayButtonOutput: false,
+});
